7a0bcdfa110d36e19192530dab1142b85556ad55
David Seres <dseres001@gmail.com>
2021-04-10 23:05:48 +0200

    small step in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-//r#![warn(missing_docs)]
+#![warn(missing_docs)]
+
+//! # rcs-parser
+//! Parsing RCS ([Revision Control System](https://www.gnu.org/software/rcs/)) files.
+//! 
+//! [Grammar](https://www.gnu.org/software/rcs/manual/html_node/comma_002dv-grammar.html#comma_002dv-grammar) of RCS files is quite simple, so this parser was easily implemented with the [Nom](https://github.com/Geal/nom) parser combinator library. 
+//! 
+//! NOTE:
+//! This project is under heavy development. Current status is **30%**.
+//! 
+//! Currently revision numbers, delta texts, delta and strings can be parsed with this library. RCS admin section is missing.
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
